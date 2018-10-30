8b337dcf638bb578b679b193c5076de6ad423ef6
Daniel Bennett and Katrina Hockman <katrina@>
2018-10-30 16:24:47 -0700

    Add?

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function Deposit(clientName, initialDeposit) {
+  this.clientName = clientName,
+  this.initialDeposit = initialDeposit
+}
+
+Deposit.prototype.newDeposit = function () {
+   // add to inital deposit
+};
+
+Deposit.prototype.withdrawal = function () {
+  /// subtract from initial dposit
+}
